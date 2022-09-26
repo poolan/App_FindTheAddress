660d6ecb2591514a27bc1fe13577482e906ddf88
raulpoolan <raulpoolan@gmail.com>
2022-09-26 17:48:12 +0300

    in progress

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
+  const [address, setAddress] = useState("");
+  const [apiKey, setApiKey] = useState("yT99ha9Y9Xeas2k2eBwAKvAOOKz6CZXK");
+  const [region, setRegion] = useState({
+    latitude: 60.200692,
+    longitude: 24.934302,
+    latitudeDelta: 0.0322,
+    longitudeDelta: 0.0221,
+  });
+
+  const showAddress = () => {
+    fetch(
+      `http://www.mapquestapi.com/geocoding/v1/address?key=${apiKey}&location=${address}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setRegion({
+          ...region,
+          latitude: data.results[0].locations[0].latLng.lat,
+          longitude: data.results[0].locations[0].latLng.lng,
+        });
+      })
+      .catch((error) => {
+        Alert.alert("Error", error);
+      });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={{ flex: 5, marginBottom: 50 }} region={region}>
+        <Marker
+          coordinate={{
+            latitude: region.latitude,
+            longitude: region.longitude,
+          }}
+        />
+      </MapView>
+      <TextInput
+        style={{
+          fontSize: 18,
+        }}
+        placeholder="address"
+        onChangeText={(address) => setAddress(address)}
+      />
+      <Button
+        title="SHOW"
+        onPress={showAddress}
+        style={{ marginBottom: 100 }}
+      />
     </View>
   );
 }
@@ -13,8 +58,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: 200,
+    borderWidth: 1,
+    marginBottom: 10,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
